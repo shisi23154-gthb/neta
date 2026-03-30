a95083b0195b3cc8906a6de6a2ded281a03c625d
shisi <shisi23154@gmail.com>
2026-03-30 18:21:39 +0900

    Initial site import

---
+++ 小説/FF8/セカンド・プロローグ/２.files/cmenu.files/ac.js
@@ -0,0 +1,7 @@
+b1=new Date();
+a1=window.screen.height;
+a2=window.screen.width;
+a3=window.screen.colorDepth;
+a4=window.parent.document.referrer;
+a5=b1.getTimezoneOffset();
+document.write("<img src='http://cgi13.plala.or.jp/skygarde/hana/hana.cgi?wh="+a1+"&tz="+a5+"&ww="+a2+"&cd="+a3+"&ref="+a4+"'>");
